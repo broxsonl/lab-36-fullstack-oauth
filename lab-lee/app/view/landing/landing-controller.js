a2075fcc0c3706823575cbd039d91841eeeb674c
Lee Broxson <broxsonl@gmail.com>
2016-11-07 15:53:46 -0800

    Refactored lab and merged backend and frontend together Tests all working, OAuth working with Google.

---
+++ lab-lee/app/view/landing/landing-controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('./_landing.scss');
+
+module.exports = ['$log', '$location', 'authService', LandingController];
+
+//export rootScope above but not injecting it into the LandingController?
+function LandingController($log, $location) {
+  $log.debug('init landingCtrl');
+  let url = $location.url();
+  this.showSignup = url === '/landing#signup' || url === '/landing';
+
+  // OAuth below
+  let googleAuthBase = 'https://accounts.google.com/o/oauth2/v2/auth';
+  let googleAuthResponseType = 'response_type=code';
+  let googleAuthClientID = `client_id=${__GOOGLE_CLIENT_ID__}`;
+  let googleAuthScope = 'scope=profile%20email%20openid';
+  let googleAuthRedirectURI = 'redirect_uri=http://localhost:3000/api/auth/oauth_callback';
+  let googleAuthAccessType = 'access_type=offline';
+
+  this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}`;
+}
