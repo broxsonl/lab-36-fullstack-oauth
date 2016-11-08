aaac09f89ea174c423e82af8eb26856ab32b26e9
Lee Broxson <broxsonl@gmail.com>
2016-11-08 13:39:16 -0800

    Followed along with class lecture and have working OAuth with redirect.

--- lab-lee/app/view/landing/landing-controller.js
+++ lab-lee/app/view/landing/landing-controller.js
@@ -2,13 +2,31 @@
 
 require('./_landing.scss');
 
-module.exports = ['$log', '$location', 'authService', LandingController];
+module.exports = ['$log', '$rootScope', '$location', 'authService', LandingController];
 
 //export rootScope above but not injecting it into the LandingController?
-function LandingController($log, $location) {
+function LandingController($log, $rootScope, $location, authService) {
   $log.debug('init landingCtrl');
   let url = $location.url();
+  let query = $location.search();
   this.showSignup = url === '/landing#signup' || url === '/landing';
+
+  if(query.token) {
+    authService.setToken(query.token)
+    .then( () => {
+      $location.path('/#/landing');
+    });
+  }
+
+  $rootScope.$on('locationChangeSuccess', () => {
+    let query = $location.search();
+    if(query.token) {
+      authService.setToken(query.token)
+      .then( () => {
+        $location.path('/#/landing');
+      });
+    }
+  });
 
   // OAuth below
   let googleAuthBase = 'https://accounts.google.com/o/oauth2/v2/auth';
@@ -16,7 +34,8 @@ function LandingController($log, $location) {
   let googleAuthClientID = `client_id=${__GOOGLE_CLIENT_ID__}`;
   let googleAuthScope = 'scope=profile%20email%20openid';
   let googleAuthRedirectURI = 'redirect_uri=http://localhost:3000/api/auth/oauth_callback';
-  let googleAuthAccessType = 'access_type=offline';
+  let googleAuthAccessType = 'access_type=offline'; // allows us to get a refresh token
+  let googleAuthPrompt = 'prompt=consent';
 
-  this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}`;
+  this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}&${googleAuthPrompt}`;
 }
